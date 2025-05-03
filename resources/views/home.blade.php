7baadfc055575d0de98885687c14e309f96fc630
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2025-05-03 20:09:05 +0100

    fiddle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
             <h2 class="text-2xl mb-2">{{ $appName }}</h2>
             <p>Laravel Version : {{ $laravelVersion }}</p>
             <p>PHP Version : {{ $phpVersion }}</p>
+            <p>Last fiddled with : 03/05/2025</p>
         </div>
     </x-slot>
     <x-slot name="page"></x-slot>
